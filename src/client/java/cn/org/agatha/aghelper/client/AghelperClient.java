ebad1c6b640ce2eff9d45bc3c2ba606375d5860c
MikeWu597 <i@hyp.ink>
2025-07-17 11:49:26 +0800

    feat: 项目启动

---
+++ src/client/java/cn/org/agatha/aghelper/client/AghelperClient.java
@@ -0,0 +1,10 @@
+package cn.org.agatha.aghelper.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AghelperClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/cn/org/agatha/aghelper/Aghelper.java
@@ -0,0 +1,10 @@
+package cn.org.agatha.aghelper;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Aghelper implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
