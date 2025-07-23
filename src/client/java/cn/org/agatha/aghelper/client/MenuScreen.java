a6aef8f0664fc2e4f0c5d4be53a89998b131ad0c
MikeWu597 <i@hyp.ink>
2025-07-23 17:57:34 +0800

    feat: 添加Logo

--- src/client/java/cn/org/agatha/aghelper/client/MenuScreen.java
+++ src/client/java/cn/org/agatha/aghelper/client/MenuScreen.java
@@ -1,9 +1,13 @@
 package cn.org.agatha.aghelper.client;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.minecraft.util.Identifier;
 
 public class MenuScreen extends Screen {
     protected MenuScreen() {
@@ -12,7 +16,6 @@ public class MenuScreen extends Screen {
 
     @Override
     protected void init() {
-        
         addDrawableChild(ButtonWidget.builder(
             Text.of("快捷键设置"),
             button -> this.client.setScreen(new KeybindSettingScreen())
@@ -28,6 +31,29 @@ public class MenuScreen extends Screen {
                 button -> this.client.setScreen(new Autologin())
         ).dimensions(width/2-75, height/2+30, 150, 20).build());
 
+
+        ScreenEvents.afterRender(this).register((_screen, drawContext, mouseX, mouseY, tickDelta) -> {
+            // 直接渲染图片
+            drawContext.drawTexture(
+                    Identifier.of("aghelper", "textures/assets/Ag_0404.png"),
+                    width/2 - 25,
+                    height/2 - 90,
+                    0, 0,
+                    50, 50,
+                    50, 50
+            );
+            // 新增：右下角显示版本号
+            String version = "Version 1.0.0";
+            int textWidth = MinecraftClient.getInstance().textRenderer.getWidth(version);
+            drawContext.drawText(
+                    MinecraftClient.getInstance().textRenderer,
+                version,
+                drawContext.getScaledWindowWidth() - textWidth - 10,
+                drawContext.getScaledWindowHeight() - 20,
+                0xFFFFFF,
+                true
+            );
+        });
     }
 
     @Override
