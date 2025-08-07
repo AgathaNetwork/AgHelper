877a3a5da1dfef48e809576c2e019e8553f83848
MikeWu597 <i@hyp.ink>
2025-08-07 23:04:14 +0800

    feat: 资源管理页面内容控件

--- src/client/java/cn/org/agatha/aghelper/client/utils/Supplies.java
+++ src/client/java/cn/org/agatha/aghelper/client/utils/Supplies.java
@@ -6,15 +6,34 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.gui.widget.ScrollableWidget;
 import net.minecraft.text.Text;
+import com.google.gson.Gson;
+import net.minecraft.util.Formatting;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Supplies extends Screen {
     public Supplies() {
         super(Text.of("资源管理"));
     }
     private ScrollableWidget scrollableWidget;
-    public int entryCount = 20;
+    private static final Gson GSON = new Gson();
+    public int entryCount = 0;
+    public boolean loaded = false;
+    public List<ListData> centerListData = new ArrayList<>();
+    public List<ListData> producerListData = new ArrayList<>();
+    public List<ListData> storageListData = new ArrayList<>();
+    public String LIST_API = "https://api-openid.agatha.org.cn/supply/getSupplyList";
 
+    public int pickedId = -1;
     @Override
     protected void init() {
         // 添加一个返回按钮
@@ -26,9 +45,9 @@ public class Supplies extends Screen {
         this.scrollableWidget = new ScrollableWidget(
                 20,  // x位置
                 40,  // y位置
-                200,                   // 宽度
+                100,                   // 宽度
                 height - 60,           // 高度
-                Text.literal("Scrollable Content")
+                Text.literal("")
         ) {
             @Override
             protected void appendClickableNarrations(NarrationMessageBuilder builder) {
@@ -54,21 +73,103 @@ public class Supplies extends Screen {
         };
 
         this.addDrawableChild(scrollableWidget);
+
+        // 异步执行，发送 HTTP GET
+        new Thread(() -> {
+
+            if (loaded == false){
+
+                try {
+                    URL url = new URL(LIST_API);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.connect();
+                    int responseCode = connection.getResponseCode();
+                    if (responseCode == 200) {
+                        loaded = true;
+                        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+                        StringBuilder response = new StringBuilder();
+                        String line;
+                        while ((line = in.readLine()) != null) {
+                            response.append(line);
+                        }
+                        in.close();
+                        String json = response.toString();
+                        // 处理JSON数据
+                        ResponseData responseData = GSON.fromJson(json, ResponseData.class);
+                        // 获取条目数
+                        entryCount = responseData.data.length;
+                        ListData[] listData = responseData.data;
+                        for (ListData data : listData) {
+                            if (data.type.equalsIgnoreCase("center")){
+                                // 添加到center
+                                centerListData.add(data);
+                            }
+                            else if (data.type.equalsIgnoreCase("producer")){
+                                // 添加到producer
+                                producerListData.add(data);
+                            }
+                            else if (data.type.equalsIgnoreCase("storage")){
+                                // storage
+                                storageListData.add(data);
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+    private static class ResponseData {
+        ListData[] data;
+    }
+    private static class ListData {
+        String id;
+        String content;
+        String type;
+        String status;
+
     }
     private void renderScrollableContent(DrawContext context, int mouseX, int mouseY, float delta) {
         // 渲染您的滚动内容
         TextRenderer textRenderer = this.textRenderer;
 
-        for (int i = 0; i < entryCount; i++) {
+        int offset = 62;
+        context.drawText(textRenderer,
+                Text.literal("储存中心").formatted(Formatting.AQUA),
+                30, offset - 12, 0xFF000000, false);
+        for (int i = 0; i < centerListData.size(); i++) {
             context.drawText(textRenderer,
-                    Text.literal("滚动内容行 " + (i + 1)),
-                    30, i * 12 + 50, 0xFFFFFF, false);
+                    Text.literal(centerListData.get(i).content),
+                    40, i * 12 + offset, 0xFFFFFF, false);
         }
+        offset += 12 * centerListData.size() + 20;
+        context.drawText(textRenderer,
+                Text.literal("生产").formatted(Formatting.AQUA),
+                30, offset - 12, 0xFF000000, false);
+        for (int i = 0; i < producerListData.size(); i++){
+            context.drawText(textRenderer,
+                    Text.literal(producerListData.get(i).content),
+                    40, i * 12 + offset, 0xFFFFFF, false);
+        }
+        offset += 12 * producerListData.size() + 20;
+        context.drawText(textRenderer,
+                Text.literal("库存").formatted(Formatting.AQUA),
+                30, offset - 12, 0xFF000000, false);
+        for (int i = 0; i < storageListData.size(); i++){
+            context.drawText(textRenderer,
+                    Text.literal(storageListData.get(i).content),
+                    40, i * 12 + offset, 0xFFFFFF, false);
+        }
+
     }
 
     private int calculateContentHeight() {
         // 计算内容总高度
-        return entryCount * 12 + 10;
+        return entryCount * 12 + 80;
     }
 
     @Override
