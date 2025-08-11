2346e510204158965f19e81865975abe74da6391
MikeWu597 <i@hyp.ink>
2025-08-11 16:33:33 +0800

    fix: 快速切换服务器时发生故障

--- src/client/java/cn/org/agatha/aghelper/client/MenuScreen.java
+++ src/client/java/cn/org/agatha/aghelper/client/MenuScreen.java
@@ -112,7 +112,7 @@ public class MenuScreen extends Screen {
                         // 然后连接到测试服
                         ServerInfo serverInfo = new ServerInfo("Agatha纯净生存测试服", "doris.agatha.org.cn", ServerInfo.ServerType.OTHER);
                         // 连接到服务器
-                        ConnectScreen.connect(client.currentScreen, client, ServerAddress.parse("doris.agatha.org.cn"), serverInfo,  false, null);
+                        ConnectScreen.connect(null, client, ServerAddress.parse("doris.agatha.org.cn"), serverInfo,  false, null);
 
                     }
             );
@@ -131,7 +131,7 @@ public class MenuScreen extends Screen {
                         // 然后连接到测试服
                         ServerInfo serverInfo = new ServerInfo("Agatha纯净生存", "agatha.org.cn", ServerInfo.ServerType.OTHER);
                         // 连接到服务器
-                        ConnectScreen.connect(client.currentScreen, client, ServerAddress.parse("agatha.org.cn"), serverInfo,  false, null);
+                        ConnectScreen.connect(null, client, ServerAddress.parse("agatha.org.cn"), serverInfo,  false, null);
 
                     }
             );
